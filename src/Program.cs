c4fc60ba3d75333c02b6d68a2ed6ae655ac1f456
HARDIntegral <mohammed.a.tazwar04@gmail.com>
2021-11-07 00:42:26 -0400

    added convenience scripts

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace CppBind
 {
     class Program
     {
-        static void Main(string[] args)
+        [DllImport(@"./CPP_SRC/libmandelbrot.so")]
+        public static extern IntPtr CalcMandelbrot(int width, int height, int granularity);
+        [DllImport(@"./CPP_SRC/libmandelbrot.so")]
+        public static extern void DeleteMandelbrot(IntPtr array);
+
+        public static void Wrapper(int width, int height, int granularity)
         {
-            Mandelbrot_Wrapper.Wrapper(60);
+            // create a bitmap of the mandelbrot set
+            IntPtr raw_buff = CalcMandelbrot(width, height, granularity);
+
+            // convert C++ array
+            int[] buff = new int[width * height];
+            Marshal.Copy(raw_buff, buff, 0, width * height);
+            DeleteMandelbrot(raw_buff);
+
+            foreach (int i in buff)
+                Console.WriteLine(i);
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Wrapper(60, 40, 60);
         }
     }
 }
